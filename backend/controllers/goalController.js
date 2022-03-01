a3e2f4a302c2e87684d5a792fe6fac8c6ec324f3
Justin Pierce Tucker <57789046+justin1732@users.noreply.github.com>
2022-03-01 15:17:00 -0600

    Getting started.

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,22 @@
+const getGoals = (req, res) => {
+    res.status(200).json({message: 'Get goals'})
+}
+
+const setGoal = (req, res) => {
+    if(!req.body.text){
+        res.status(400)
+    }
+    res.status(200).json({message: 'Set goals'})
+}
+
+const updateGoals = (req, res) => {
+    res.status(200).json({message: `Updated goal ${req.params.id}`} )
+}
+
+const deleteGoal = (req, res) => {
+    res.status(200).json({message: `Deleted goal ${req.params.id}`} )
+}
+
+module.exports = {
+    getGoals, setGoal, updateGoals, deleteGoal
+}
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log ("Hello world! Back")
+const express = require('express')
+const dotenv = require ('dotenv').config
+const port = process.env.PORT || 5000
+
+const app = express ()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.listen(port, () => console.log (`Backend server listening on port ${port}`) )
