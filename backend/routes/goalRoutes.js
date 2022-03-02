cb06dc9196353192f66ab7bbfd7d57b6508083f1
Justin Pierce Tucker <57789046+justin1732@users.noreply.github.com>
2022-03-02 15:38:10 -0600

    Added up models.

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,9 +1,9 @@
 const route = require('color-convert/route')
 const express = require('express')
 const router = express.Router()
-const { getGoals, setGoal, updateGoals, deleteGoal } = require('../controllers/goalController')
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
 
 router.route('/').get(getGoals).post(setGoal)
-router.route('/:id').delete(deleteGoal).put(updateGoals)
+router.route('/:id').delete(deleteGoal).put(updateGoal)
 
 module.exports = router
